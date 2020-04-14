f4caabc4b22277bfaac1c7d2e73640214f40e16b
Rathna <rnatarajan@axway.com>
2020-04-14 00:31:29 -0700

    log and kibana url change

--- frontend/src/Pages/Iframe/Iframe.js
+++ frontend/src/Pages/Iframe/Iframe.js
@@ -5,11 +5,17 @@ class Iframe extends React.Component{
     render(){
         return(
             <div style={{position:'relative', top:'20px', left:'0',height:'70vh'}}>
-              <iframe id="inlineFrameExample"
+              <!--iframe id="inlineFrameExample"
                 title="Inline Frame Example"
                 style={{width:'100%', height:'100%', border:'none', margin:'0', padding:'0', overflow:'hidden', zIindex:"999999"}}
                 src="https://search-runners-jy4ck5nrfdbtqohuhuhefdsetq.eu-west-1.es.amazonaws.com/_plugin/kibana/app/kibana?security_tenant=private#/dashboard/318f0400-7ce6-11ea-989a-87c1bc1b5c07?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow%2Fw%2Cto%3Anow%2Fw))">
-                </iframe>
+                </iframe-->
+
+                <iframe id="inlineFrameExample"
+               title="Inline Frame Example"
+               style={{width:'100%', height:'100%', border:'none', margin:'0', padding:'0', overflow:'hidden', zIindex:"999999"}}
+               src="http://fit-together.demo.axway.com:8443/goto/d3b931da10bcd8bb03f9849aafe94aa1?embed=true">
+               </iframe>
             </div>
         )
     }
