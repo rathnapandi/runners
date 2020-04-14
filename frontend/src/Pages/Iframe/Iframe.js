2e456e4f0fe8c01d12e8a201758a6dc1fb9e2798
Rathna <rnatarajan@axway.com>
2020-04-14 00:40:48 -0700

    log and kibana url change

--- frontend/src/Pages/Iframe/Iframe.js
+++ frontend/src/Pages/Iframe/Iframe.js
@@ -8,7 +8,7 @@ class Iframe extends React.Component{
               <iframe id="inlineFrameExample"
                 title="Inline Frame Example"
                 style={{width:'100%', height:'100%', border:'none', margin:'0', padding:'0', overflow:'hidden', zIindex:"999999"}}
-                src="http://fit-together.demo.axway.com:8443/goto/d3b931da10bcd8bb03f9849aafe94aa1?embed=true">
+                src="https://fit-together.demo.axway.com:8443/goto/d3b931da10bcd8bb03f9849aafe94aa1?embed=true">
                 </iframe>
             </div>
         )
